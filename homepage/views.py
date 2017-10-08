1660a5be6297ea40340f888a5a0104bf43ae76a0
August Lund Eilertsen <AugustLE@dhcp-10-22-43-244.wlan.ntnu.no>
2017-10-08 16:57:49 +0200

    Fix

--- homepage/views.py
+++ homepage/views.py
@@ -1,3 +1,16 @@
 from django.shortcuts import render
+from django.views import generic
+from django.core.urlresolvers import resolve
+import datetime
 
-# Create your views here.
+class IndexView(generic.ListView):
+
+    model = None
+    template_name = 'homepage/index.html'
+
+    id = 0
+    context_object_name = 'query_set'
+
+    def get_queryset(self):
+
+        return 0
